dfe0aa93666109599840e5ea9a17c501c0cc19d1
1emvr <1emvr@protonmail.com>
2024-07-22 06:46:33 -0400

    cleanup

--- loader/include/injectlib.hpp
+++ loader/include/injectlib.hpp
@@ -2,7 +2,7 @@
 #define _HEXANE_INJECTLIB_CONFIG_HPP
 #include "core/monolith.hpp"
 #include "core/corelib.hpp"
-#include <inject/injectlib/resource.hpp>
+#include "loader/include/resource.hpp"
 
 EXTERN_C VOID Execute();
 EXTERN_C FUNCTION VOID Entrypoint(HMODULE Base);
