cb96597aaee1dbad19ad468bdd325c5da2e3c9e9
1emvr <1emvr@protonmail.com>
2024-08-09 07:49:52 -0400

    plans to implement inline shellcode execution

--- commands/src/registry.cpp
+++ commands/src/registry.cpp
@@ -13,20 +13,20 @@ LSTATUS RegCreateSubkey(HKEY Key, LPSTR Subkey, LPSTR Name, DWORD Value) {
     HEXANE
 
     LSTATUS Result  = 0;
-    HKEY hkResult   = { };
+    HKEY hkOpen     = { };
 
-    if ((Ctx->win32.RegOpenKeyExA(Key, Subkey, 0, KEY_READ, &hkResult)) != ERROR_SUCCESS) {
-        if ((Result = Ctx->win32.RegCreateKeyExA(Key, Subkey, 0, nullptr, REG_OPTION_NON_VOLATILE, KEY_CREATE_SUB_KEY | KEY_SET_VALUE, nullptr, &hkResult, nullptr)) != ERROR_SUCCESS) {
+    if ((Ctx->win32.RegOpenKeyExA(Key, Subkey, 0, KEY_READ, &hkOpen)) != ERROR_SUCCESS) {
+        if ((Result = Ctx->win32.RegCreateKeyExA(Key, Subkey, 0, nullptr, REG_OPTION_NON_VOLATILE, KEY_CREATE_SUB_KEY | KEY_SET_VALUE, nullptr, &hkOpen, nullptr)) != ERROR_SUCCESS) {
             goto defer;
         }
     }
-    if ((Result = Ctx->win32.RegSetValueExA(hkResult, Name, 0, REG_DWORD, (CONST PBYTE)&Value, sizeof(DWORD))) != ERROR_SUCCESS) {
+    if ((Result = Ctx->win32.RegSetValueExA(hkOpen, Name, 0, REG_DWORD, (CONST PBYTE)&Value, sizeof(DWORD))) != ERROR_SUCCESS) {
         goto defer;
     }
 
     defer:
-    if (hkResult) {
-        Ctx->win32.RegCloseKey(hkResult);
+    if (hkOpen) {
+        Ctx->win32.RegCloseKey(hkOpen);
     }
 
     return Result;
