44ee313a43d1eb1a29ec46090641dd84f4ec757f
1emvr <1emvr@protonmail.com>
2024-08-09 07:49:52 -0400

    plans to implement inline shellcode execution

--- core/dotnet.hpp
+++ core/dotnet.hpp
@@ -1,4 +1,5 @@
 #ifndef HEXANE_IMPLANT_DOTNET_HPP
 #define HEXANE_IMPLANT_DOTNET_HPP
 #include "C:\Program Files (x86)\Windows Kits\NETFXSDK\4.8\Include\um\metahost.h"
+#include "C:\Program Files (x86)\Windows Kits\NETFXSDK\4.8\Include\um\mscoree.h"
 #endif //HEXANE_IMPLANT_DOTNET_HPP
