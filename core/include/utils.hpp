fd38a97b60171ec35499433b49e15df997f4ddb9
Lemur <103511211+1emvr@users.noreply.github.com>
2024-08-22 07:58:20 -0400

    Update monolith.hpp

--- core/include/utils.hpp
+++ core/include/utils.hpp
@@ -7,10 +7,10 @@
 #define MINUTES(x)      (x * SECONDS(64))
 
 #define FNV_OFFSET      (const unsigned int) 2166136261
-#define FNV_PRIME	(const unsigned int) 16777619
+#define FNV_PRIME	    (const unsigned int) 16777619
 
-#define WNULTERM 	0x00000000
-#define NULTERM		0x00
+#define WNULTERM 	    0x00000000
+#define NULTERM		    0x00
 #define PERIOD          0x2E
 #define BSLASH          0x5C
 #define ASTER           0x2A
@@ -18,24 +18,23 @@
 #include <core/corelib.hpp>
 
 namespace Utils {
-
     FUNCTION VOID AppendBuffer(uint8_t **buffer, const uint8_t *target, uint32_t *capacity, uint32_t length);
     FUNCTION VOID AppendPointerList(void **array[], void *pointer, uint32_t *count);
-    FUNCTION ULONG GetHashFromStringW(WCHAR CONST *String, SIZE_T Length);
-    FUNCTION ULONG GetHashFromStringA(CHAR CONST *String, SIZE_T Length);
+    FUNCTION ULONG GetHashFromStringA(char const *string, size_t length);
+    FUNCTION ULONG GetHashFromStringW(wchar_t const *string, size_t length);
 
     namespace Time {
         FUNCTION ULONG64 GetTimeNow();
         FUNCTION BOOL InWorkingHours();
+        FUNCTION VOID Timeout(size_t ms);
     }
 
     namespace Random {
-        FUNCTION VOID Timeout(size_t ms);
+        FUNCTION ULONG RandomSleepTime();
+        FUNCTION ULONG RandomSeed();
         FUNCTION UINT_PTR Timestamp();
         FUNCTION ULONG RandomNumber32();
-        FUNCTION ULONG RandomSleepTime();
         FUNCTION BOOL RandomBool();
-        FUNCTION ULONG RandomSeed();
     }
 }
 
