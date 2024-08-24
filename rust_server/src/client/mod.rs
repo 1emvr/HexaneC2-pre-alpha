ff7238adef71ebf479ed6e477a47759e0bf30f68
1emvr <1emvr@protonmail.com>
2024-08-24 11:16:55 -0400

    adding rust server

--- rust_server/src/client/mod.rs
+++ rust_server/src/client/mod.rs
@@ -1,3 +1,7 @@
+use std::fmt::Debug;
+use std::io::{Write};
+use std::io;
+
 const BANNER: &str = r#"
  _                                 ___ ____
 | |__   _____  ____ _ _ __   ___  / __\___ \
@@ -7,7 +11,7 @@ const BANNER: &str = r#"
 "#;
 
 pub struct Client {
-    // implement with crossterm
+    // todo: implement with crossterm
 }
 
 impl Client {
@@ -15,8 +19,29 @@ impl Client {
         println!("{}", BANNER);
     }
 
+    fn implants(args: Vec<&str>) {
+        println!("{}", args);
+    }
+
+    fn help() {
+        println!("help menu")
+    }
+
     pub fn run_client() {
         Self::print_banner();
+
+        let mut input = String::new();
+        while input.as_str() != "exit" {
+            println!(" > ");
+
+            io::stdin().read_line(&mut input).expect("error reading user input");
+            let args: Vec<&str> = input.split(" ").collect();
+
+            match input.as_str() {
+                "implant"   => Self::implants(args),
+                "help"      => Self::help(),
+                _ => println!("invalid input")
+            }
+        }
     }
 }
-
