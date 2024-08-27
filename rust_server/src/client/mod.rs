e14b1de231d0ef0da47af3e9b53e8929fd699872
1emvr <1emvr@protonmail.com>
2024-08-27 03:30:14 -0400

    removing linked lists. resulting to Vec<>

--- rust_server/src/client/mod.rs
+++ rust_server/src/client/mod.rs
@@ -5,11 +5,13 @@ use std::fs;
 use clap::Parser;
 
 use serde_json;
+use serde_json::Result;
+use serde_json::Result as JsonResult;
 use serde::Deserialize;
+use serde::ser::Error;
 
 use std::io::{self, Write};
 use lazy_static::lazy_static;
-use serde::de::Error;
 use crate::client::types::{Hexane, JsonData, CompilerConfig, UserSession, Args};
 
 const BANNER: &str = r#"
@@ -26,17 +28,13 @@ lazy_static! {
     pub(crate) static ref SHOW_COMPILER: bool  = ARGS.show_compiler;
 }
 
-fn print_banner() {
-    println!("{}", BANNER);
-}
-
 fn cursor() {
     print!(" > ");
     io::stdout().flush().unwrap();
 }
 
 fn init() {
-    print_banner();
+    println!("{}", BANNER);
 
     if *DEBUG { println!("running in debug mode") }
     if *SHOW_COMPILER { println!("running with compiler output") }
@@ -80,51 +78,50 @@ pub fn run_client() {
     }
 }
 
-fn map_json_config(file_path: &String) -> Result<Hexane, dyn Error> {
+fn map_json_config(file_path: &String) -> Result<Hexane> {
     let json_file = "./json/".to_owned() + file_path.as_str();
 
-    match fs::read_to_string(json_file) {
-        Ok(contents) => {
-
-            match serde_json::from_str(contents.as_str()).expect("invalid json syntax") {
-                Ok(json_data) => {
-
-                    let group_id = 0;
-                    let instance = Hexane {
-
-                        current_taskid: 0, peer_id: 0, group_id, build_type: 0, network_type: 0,
-                        crypt_key: vec![], shellcode: vec![], config_data: vec![],
-                        active: false,
-
-                        compiler: CompilerConfig {
-                            file_extension:     String::from(""),
-                            build_directory:    String::from(""),
-                            compiler_flags:     vec![],
-                        },
-
-                        main:       json_data.config,
-                        network:    json_data.network,
-                        builder:    json_data.builder,
-                        loader:     json_data.loader,
-
-                        user_session: UserSession {
-                            username: String::from(""),
-                            is_admin: false,
-                        },
-                    };
-                    Ok(instance)
-                }
-                Err(err) => {
-                    eprintln!("error reading json data: {err}");
-                    Err(err)
-                }
-            }
+    let contents = match fs::read_to_string(json_file.as_str()).expect("file path error") {
+        Ok(contents) => contents,
+        Err(e) => {
+            eprintln!("error reading file {json_file}: {e}");
+            Err(e)
         }
-        Err(err) => {
-            eprintln!("error opening {}: {}", file_path, err);
-            Err(Error::custom(format!("failed to read config file: {}", err)))
+    };
+
+    let json_data = match serde_json::from_str(contents.as_str()).expect("invalid json syntax") {
+        Ok(json_data) => json_data,
+        Err(e) => {
+            eprintln!("error reading json data: {e}");
+            Err(e)
         }
-    }
+    };
+
+    let group_id = 0;
+    let instance = Hexane {
+
+        current_taskid: 0, peer_id: 0, group_id, build_type: 0, network_type: 0,
+        crypt_key: vec![], shellcode: vec![], config_data: vec![],
+        active: false,
+
+        compiler: CompilerConfig {
+            file_extension:     String::from(""),
+            build_directory:    String::from(""),
+            compiler_flags:     vec![],
+        },
+
+        main:       json_data.config,
+        network:    json_data.network,
+        builder:    json_data.builder,
+        loader:     json_data.loader,
+
+        user_session: UserSession {
+            username: String::from(""),
+            is_admin: false,
+        },
+    };
+
+    Ok(instance)
 }
 
 fn setup_instance(instance: &mut Hexane) {
