1d3ca19e205944f8f96f4ca873089f69b69ac177
1emvr <1emvr@protonmail.com>
2024-08-27 03:30:14 -0400

    removing linked lists. resulting to Vec<>

--- rust_server/src/client/mod.rs
+++ rust_server/src/client/mod.rs
@@ -5,11 +5,11 @@ use std::fs;
 use clap::Parser;
 
 use serde_json;
-use serde_json::Error;
 use serde::Deserialize;
 
 use std::io::{self, Write};
 use lazy_static::lazy_static;
+use serde::de::Error;
 use crate::client::types::{Hexane, JsonData, CompilerConfig, UserSession, Args};
 
 const BANNER: &str = r#"
@@ -58,18 +58,71 @@ pub fn run_client() {
         }
 
         let args: Vec<String> = input.split_whitespace().map(str::to_string).collect();
+
         match args[0].as_str() {
             "load" => {
-                let mut instance = map_json_config(&args[1]).expect("TODO: panic message");
+                match map_json_config(&args[1]) {
+                    Ok(mut instance) => {
+                        setup_instance(&mut instance);
+                        setup_server(&instance);
 
-                setup_instance(&mut instance);
-                setup_server(&instance);
-
-                instances.push(instance);
+                        instances.push(instance);
+                    }
+                    Err(err)=> {
+                        println!("failed to load json config: {err}")
+                    }
+                }
             },
 
             "exit" => break,
             _ => println!("invalid input")
+        }
+    }
+}
+
+fn map_json_config(file_path: &String) -> Result<Hexane, dyn Error> {
+    let json_file = "./json/".to_owned() + file_path.as_str();
+
+    match fs::read_to_string(json_file) {
+        Ok(contents) => {
+
+            match serde_json::from_str(contents.as_str()).expect("invalid json syntax") {
+                Ok(json_data) => {
+
+                    let group_id = 0;
+                    let instance = Hexane {
+
+                        current_taskid: 0, peer_id: 0, group_id, build_type: 0, network_type: 0,
+                        crypt_key: vec![], shellcode: vec![], config_data: vec![],
+                        active: false,
+
+                        compiler: CompilerConfig {
+                            file_extension:     String::from(""),
+                            build_directory:    String::from(""),
+                            compiler_flags:     vec![],
+                        },
+
+                        main:       json_data.config,
+                        network:    json_data.network,
+                        builder:    json_data.builder,
+                        loader:     json_data.loader,
+
+                        user_session: UserSession {
+                            username: String::from(""),
+                            is_admin: false,
+                        },
+                    };
+                    Ok(instance)
+                }
+                Err(err) => {
+                    eprintln!("error reading json data: {err}");
+                    Err(err)
+                }
+            }
+        }
+        Err(err) => {
+            eprintln!("error opening {}: {}", file_path, err);
+            Err(Error::custom(format!("failed to read config file: {}", err)))
         }
     }
 }
@@ -81,37 +134,4 @@ fn setup_instance(instance: &mut Hexane) {
 fn setup_server(instance: &Hexane) {
 
 }
-
-fn map_json_config(file_path: &String) -> Result<Hexane, Error> {
-
-    let contents = fs::read_to_string("./json/".to_owned() + file_path.as_str()).expect("invalid file path");
-    let json_data: JsonData = serde_json::from_str(contents.as_str()).expect("invalid json syntax");
-
-    let group_id = 0;
-    let instance = Hexane {
-
-        current_taskid: 0, peer_id: 0, group_id, build_type: 0, network_type: 0,
-        crypt_key: vec![], shellcode: vec![], config_data: vec![],
-        active: false,
-
-        compiler: CompilerConfig {
-            file_extension:     String::from(""),
-            build_directory:    String::from(""),
-            compiler_flags:     vec![],
-        },
-
-        main:       json_data.config,
-        network:    json_data.network,
-        builder:    json_data.builder,
-        loader:     json_data.loader,
-
-        user_session: UserSession {
-            username: String::from(""),
-            is_admin: false,
-        },
-    };
-
-    Ok(instance)
-}
-
 
