cff3a693adec833c8167f88b2f4489a645c68bee
1emvr <1emvr@protonmail.com>
2024-08-28 01:40:13 -0400

    adding custom error handling

--- rust_server/src/client/error.rs
+++ rust_server/src/client/error.rs
@@ -3,7 +3,7 @@ use derive_more::From;
 
 pub type Result<T> = core::result::Result<T, Error>;
 
-#[derive(Debug, serde::Deserialize, From)]
+#[derive(Debug, From)]
 pub enum Error {
     #[from]
     Custom(String),
