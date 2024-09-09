303f19815fe3e7a8cd21c48a1ad1bb174875c03c
1emvr <1emvr@protonmail.com>
2024-09-09 10:40:25 -0400

    rewriting bof loader

--- rust_server/src/server/builder.rs
+++ rust_server/src/server/builder.rs
@@ -37,7 +37,7 @@ pub fn generate_definitions(definitions: HashMap<String, Vec<u8>>) -> String {
     defs
 }
 
-pub fn compile_object(instance: Hexane) -> Result<()> {
+pub fn compile_object(mut instance: Hexane, flags: Vec<String>) -> Result<()> {
 
     if instance.main.debug {
         if instance.compiler.command != "ld" && instance.compiler.command != "nasm" {
@@ -56,25 +56,25 @@ pub fn compile_object(instance: Hexane) -> Result<()> {
     }
 
     if !instance.components.is_empty() {
-        instance.command += &generate_arguments(instance.components.clone());
+        instance.compiler.command += &generate_arguments(instance.components.clone());
     }
-    if !instance.flags.is_empty() {
-        instance.command += &generate_arguments(instance.flags.clone());
+    if !instance.compiler.compiler_flags.is_empty() {
+        instance.compiler.command += &generate_arguments(instance.compiler.compiler_flags.clone());
     }
 
     for (k, v) in &instance.definitions {
-        instance.command += &generate_definitions(HashMap::from([(k.clone(), v.clone())]));
+        instance.compiler.command += &generate_definitions(HashMap::from([(k.clone(), v.clone())]));
     }
 
-    instance.command += &format!(" -o {} ", instance.output_name);
-    run_command(&instance.command, &instance.peer_id.to_string())?;
+    instance.compiler.command += &format!(" -o {} ", instance.builder.output_name);
+    run_command(&instance.compiler.command, &instance.peer_id.to_string())?;
 
     wrap_message("debug", &"embedding config data".to_owned());
-    embed_section_data(&instance.output_name, ".text$F", &instance.config_data.as_slice())?;
+    embed_section_data(&instance.builder.output_name, ".text$F", &instance.config_data.as_slice())?;
 }
 
-pub fn compile_sources(&self, root_directory: &str) -> Result<()> {
-    let src_path        = Path::new(root_directory).join("src");
+pub fn compile_sources(instance: Hexane) -> Result<()> {
+    let src_path        = Path::new(&instance.builder.root_directory).join("src");
     let entries: Vec<_> = fs::read_dir(src_path)?.filter_map(|entry| entry.ok()).collect();
 
     let atoms = Arc::new(Mutex::new(()));
@@ -86,13 +86,10 @@ pub fn compile_sources(&self, root_directory: &str) -> Result<()> {
         }
 
         let path    = src.path();
-        let output  = Path::new(&env::current_dir().unwrap().join("build")).join(self.output_name.clone()).with_extension("o");
+        let output  = Path::new(&env::current_dir().unwrap().join("build")).join(instance.builder.output_name.clone()).with_extension("o");
 
         let atoms_clone     = Arc::clone(&atoms);
         let err_clone       = err_send.clone();
-        let inc_clone       = self.includes.clone();
-        let mut components  = self.components.clone();
-        let def_clone       = self.definitions.clone();
 
         let result = {
             let _guard = atoms_clone.lock().unwrap();
@@ -102,45 +99,16 @@ pub fn compile_sources(&self, root_directory: &str) -> Result<()> {
                     wrap_message("debug", &format!("compiling {}", &output.display()));
 
                     let flags = vec!["-f win64".to_string()];
-
-                    let target = CompileTarget {
-                        architecture:   "".to_string(),
-                        command:        "nasm".to_string(),
-                        output_name:    output.to_string_lossy().to_string(),
-                        components:     vec![path.to_string_lossy().to_string()],
-                        includes:       Some(vec![]),
-                        network_type:   NetworkType::Http,
-                        definitions:    HashMap::new(),
-                        config_data:    self.config_data.clone(),
-                        peer_id:        self.peer_id,
-                        debug:          self.debug,
-                        flags,
-                    };
-
-                    target.compile_object()
+                    compile_object(instance, flags)?;
                 }
 
                 Some("cpp") => {
                     wrap_message("debug", &format!("compiling {}", &output.display()));
 
-                    let mut flags = self.flags.clone();
-                    flags.push("-c".to_string());
+                    let mut flags = instance.compiler.compiler_flags.clone();
+                    flags.push("-c");
 
-                    let target = CompileTarget {
-                        command:        "x86_64-w64-mingw32-g++".to_string(),
-                        output_name:    output.to_string_lossy().to_string(),
-                        includes:       inc_clone,
-                        definitions:    def_clone,
-                        peer_id:        self.peer_id,
-                        debug:          self.debug,
-                        components:     vec![path.to_string_lossy().to_string()],
-                        config_data:    vec![],
-                        flags,
-                        architecture: "".to_string(),
-                        network_type: NetworkType::Http,
-                    };
-
-                    target.compile_object()
+                    compile_object(instance, flags);
                 }
                 _ => Ok(()),
             }
