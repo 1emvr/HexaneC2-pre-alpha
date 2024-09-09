df66ea38aaeb4211a2c16688c37c755160535248
1emvr <1emvr@protonmail.com>
2024-09-09 11:05:05 -0400

    send all compiler code to Hexane class

--- rust_server/src/server/builder.rs
+++ rust_server/src/server/builder.rs
@@ -1,15 +1,16 @@
-use rayon::prelude::*;
 use std::collections::HashMap;
 use std::{env, fs};
 use std::path::Path;
 use std::sync::{Arc, Mutex};
 use std::sync::mpsc::channel;
-use crate::return_error;
+
+use rayon::prelude::*;
+use crate::server::instance::Hexane;
 use crate::server::error::{Error, Result};
 use crate::server::binary::embed_section_data;
-use crate::server::instance::Hexane;
 use crate::server::utils::{run_command, wrap_message};
 use crate::server::types::NetworkType;
+use crate::return_error;
 
 pub fn generate_includes(includes: Vec<String>) -> String {
     wrap_message("debug", &"including directories".to_owned());
