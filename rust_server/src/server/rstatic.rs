d0085c47f481c0ba81e99b3e112eeb5b562b58bb
1emvr <1emvr@protonmail.com>
2024-09-10 05:55:04 -0400

    refactor error handling

--- rust_server/src/server/rstatic.rs
+++ rust_server/src/server/rstatic.rs
@@ -3,23 +3,26 @@ use std::path::PathBuf;
 use std::sync::Mutex;
 use clap::Parser;
 use crossbeam_channel::{unbounded, Receiver as Recv, Sender as Send};
+use lazy_static::lazy_static;
 use crate::server::session::Args;
 use crate::server::types::{Message, UserSession};
 
-pub(crate) static ARGS: Args                                = Args::parse();
-pub(crate) static CURDIR: PathBuf                           = env::current_dir().unwrap();
-pub(crate) static DEBUG: bool                               = ARGS.debug;
-pub(crate) static SHOW_COMPILER: bool                       = ARGS.show_compiler;
-pub(crate) static AMD64: String                             = String::from("amd64");
+lazy_static!(
+    pub(crate) static ref ARGS: Args                                = Args::parse();
+    pub(crate) static ref CURDIR: PathBuf                           = env::current_dir().unwrap();
+    pub(crate) static ref DEBUG: bool                               = ARGS.debug;
+    pub(crate) static ref SHOW_COMPILER: bool                       = ARGS.show_compiler;
+    pub(crate) static ref AMD64: String                             = String::from("amd64");
 
-pub(crate) static USERAGENT: String                         = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36".to_owned();
-pub(crate) static SESSION: Mutex<UserSession>               = Mutex::new(UserSession{ username: "".to_owned(), is_admin: false });
+    pub(crate) static ref USERAGENT: String                         = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36".to_owned();
+    pub(crate) static ref SESSION: Mutex<UserSession>               = Mutex::new(UserSession{ username: "".to_owned(), is_admin: false });
 
-pub(crate) static CHANNEL: (Send<Message>, Recv<Message>)   = unbounded();
-pub(crate) static EXIT: (Send<()>, Recv<()>)                = unbounded();
+    pub(crate) static ref CHANNEL: (Send<Message>, Recv<Message>)   = unbounded();
+    pub(crate) static ref EXIT: (Send<()>, Recv<()>)                = unbounded();
+);
 
-pub(crate) static STRINGS: &'static str                     = "./configs/strings.txt";
-pub(crate) static HASHES: &'static str                      = "./core/include/names.hpp";
-pub(crate) static DEBUG_FLAGS: &'static str                 = "-std=c++23 -g -Os -nostdlib -fno-asynchronous-unwind-tables -masm=intel -fno-ident -fpack-struct=8 -falign-functions=1 -ffunction-sections -fdata-sections -falign-jumps=1 -w -falign-labels=1 -fPIC -fno-builtin -Wl,--no-seh,--enable-stdcall-fixup,--gc-sections";
-pub(crate) static RELEASE_FLAGS: &'static str               = "-std=c++23 -Os -nostdlib -fno-asynchronous-unwind-tables -masm=intel -fno-ident -fpack-struct=8 -falign-functions=1 -ffunction-sections -fdata-sections -falign-jumps=1 -w -falign-labels=1 -fPIC  -fno-builtin -Wl,-s,--no-seh,--enable-stdcall-fixup,--gc-sections";
+pub(crate) static STRINGS: &'static str         = "./configs/strings.txt";
+pub(crate) static HASHES: &'static str          = "./core/include/names.hpp";
+pub(crate) static DEBUG_FLAGS: &'static str     = "-std=c++23 -g -Os -nostdlib -fno-asynchronous-unwind-tables -masm=intel -fno-ident -fpack-struct=8 -falign-functions=1 -ffunction-sections -fdata-sections -falign-jumps=1 -w -falign-labels=1 -fPIC -fno-builtin -Wl,--no-seh,--enable-stdcall-fixup,--gc-sections";
+pub(crate) static RELEASE_FLAGS: &'static str   = "-std=c++23 -Os -nostdlib -fno-asynchronous-unwind-tables -masm=intel -fno-ident -fpack-struct=8 -falign-functions=1 -ffunction-sections -fdata-sections -falign-jumps=1 -w -falign-labels=1 -fPIC  -fno-builtin -Wl,-s,--no-seh,--enable-stdcall-fixup,--gc-sections";
 
