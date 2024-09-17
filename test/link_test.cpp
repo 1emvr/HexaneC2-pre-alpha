18a2c1a6d6c6841714df142683e92d655b2830c0
1emvr <1emvr@protonmail.com>
2024-09-17 04:56:09 -0400

    -Wl,--gc-sections deletes code and screws everything up for some reason unknown

--- test/link_test.cpp
+++ test/link_test.cpp
@@ -1,16 +1,16 @@
 #include "link_test.hpp"
-//__text(F) uint8_t data[8] = { 0x41,0x41,0x41,0x41,0x41,0x41,0x41,0x41};
+__text(F) uint8_t data[8] = { 0x41,0x41,0x41,0x41,0x41,0x41,0x41,0x41};
 
-//__text(B) int func_a() {
-//    return 2*2;
-//}
-//
-//__text(B) int func_b() {
-//    return 1+1;
-//}
+__text(B) int func_a() {
+    return 2*2;
+}
+
+__text(B) int func_b() {
+    return 1+1;
+}
 
 __text(B) int Entrypoint() {
-    //func_b();
-    //func_a();
+    func_b();
+    func_a();
     return 0;
 }
