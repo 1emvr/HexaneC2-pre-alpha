ad35fa4003cee1c63d95accf94e7828d47895f2f
1emvr <1emvr@protonmail.com>
2024-09-21 10:22:41 -0400

    adding terraform scripts

---
+++ terraform/main.tf
@@ -0,0 +1,31 @@
+terraform {
+	required_providers {
+		google = {
+			source = "hashicorp/google"
+			version = "4.51.0"
+		}
+	}
+}
+
+provider "google" {
+	project = "lemur-test-terraform"
+}
+
+resource "google_compute_instance" "redirector" {
+	count = 1
+
+	name = "lemur-tests-terraform-${count.index}"
+	machine_type = "e2-micro"
+	zone = "us-east1-b"
+
+	boot_disk {
+		initialize_params {
+			image = "ubuntu-minimal-2210-kinetic-amd64-v20230126"
+		}
+	}
+
+	network_interface {
+		network = "default"
+		access_config {}
+	}
+}
