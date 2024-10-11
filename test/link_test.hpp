59d43ef9d3d76eab7f5ebe2e0272ea97d9755c84
1mevr <1emvr@protonmail.com>
2024-10-11 04:00:38 -0400

    taking time to clean up server mess

--- test/link_test.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef LINKE_TEST_HPP
-#define LINKE_TEST_HPP
-#include <core/monolith.hpp>
-__text(B) int func_a();
-__text(B) int func_b();
-extern "C" __text(B) int Entrypoint();
-#endif //LINKE_TEST_HPP
