f9c19564f99b6e5ae7d796e4793561a3234e9df5
lemur <1emvr@protonmail.com>
2024-10-29 10:35:18 -0400

    updating listener with shared memory configs

--- listener/src/main.rs
+++ listener/src/main.rs
@@ -5,6 +5,8 @@ use std::sync::{Arc, Mutex};
 use serde::{Deserialize, Serialize};
 
 use crate::types::Hexane;
+use crate::types::NetworkOptions
+
 type HexaneStore = Arc<Mutex<Option<Hexane>>>;
 
 #[tokio::main]
@@ -17,10 +19,26 @@ async fn main() {
             .and(warp::post())
             .and(warp::body::bytes()) 
             .map(move |body: bytes::Bytes| {
+
                 match bincode::deserialize::<Hexane>(&body) {
                     Ok(des) => {
                         let mut data_guard = config.lock().unwrap();
-                        *data_guard = Some(des);
+                        *data_guard = Some(des.clone());
+
+                        if let Some(network) = &des.network_cfg {
+                            match &network.options {
+
+                                NetworkOptions::Http(http) => {
+                                    let endpoints = &http.endpoints;
+                                    warp::reply::with_status(format!("http config received: {:?}", endpoints), warp::http::StatusCode::OK);
+                                }
+
+                                NetworkOptions::Smb() => {
+
+                                }
+                            }
+                        }
+
                         warp::reply::with_status("config received", warp::http::StatusCode::OK);
                     }
                     Err(e) => {
