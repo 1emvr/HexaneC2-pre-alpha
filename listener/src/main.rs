f85ff1a7c0d6af791045985d54c04c6b67c73404
lemur <1emvr@protonmail.com>
2024-10-29 09:38:25 -0400

    forget rebasing

---
+++ listener/src/main.rs
@@ -0,0 +1,38 @@
+mod types;
+
+use warp::Filter;
+use std::sync::{Arc, Mutex};
+use serde::{Deserialize, Serialize};
+
+
+#[tokio::main]
+async fn main() {
+    let config = Arc::new(Mutex::new(Config{ endpoints: vec![] }));
+
+    // TODO: might just pass the entire Hexane struct from the client 
+
+    let routes = {
+        let config = Arc::clone(&config);
+
+        warp::path("config") 
+            .and(warp::post())
+            .and(warp::body::json()) // TODO: send the SerdeJson config from client to http://ip/config during build time
+            .map(move |new_config: Config| {
+                let mut config_guard = config.lock().unwrap();     
+
+                *config_guard = new_config;
+                warp::reply::with_status("config updated", warp::http::StatusCode::OK)
+            });
+    };
+
+    let routes = warp::any()
+        .and(warp::path::tail())
+        .and(warp::method())
+        .and(warp::body::bytes())
+        .and(with_config(config.clone()))
+        .map(
+            | tail: warp::filters::path::Tail, method: warp::http::Method, body: bytes::Bytes, config: Arc<Mutex<Hexane>> | {
+
+            }
+        );
+}
