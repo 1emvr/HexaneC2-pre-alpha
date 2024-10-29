ebaceb8ee09fc0bfe09516ca4fb01e774a3eb9d4
lemur <1emvr@protonmail.com>
2024-10-29 11:30:15 -0400

    need to read more about how Warp works

--- listener/src/main.rs
+++ listener/src/main.rs
@@ -3,33 +3,66 @@ mod types;
 use warp::Filter;
 use std::sync::{Arc, Mutex};
 use serde::{Deserialize, Serialize};
+use std::collections::HashSet;
 
 use crate::types::Hexane;
 use crate::types::NetworkOptions
 
-type HexaneStore = Arc<Mutex<Option<Hexane>>>;
+type HexaneStore = Arc<Mutex<Vec<Hexane>>>;
+type EndpointStore = Ac<Mutext<HashSet<String>>>;
 
 #[tokio::main]
 async fn main() {
-    let config = Arc::new(Mutex::new(None));
-    let routes = {
-        let config_clone = Arc::clone(&config);
+    let instances: HexaneStore = Arc::new(Mutex::new(None));
+    let endpoints: EndpointStore = Arc::new(Mutex::new(HashSet::new())); 
 
-        warp::path("config") 
+    let base_route = warp::path::end()
+        .map(|| "hexane listener is running");
+
+    // NOTE: dynamic router
+    let dynamic_routes = {
+        let endpoints = Arc::clone(&endpoints);
+
+        warp::path::param::<String>()
+            .and_then(move |endpoint: String| {
+                let endpoints = endpoints.lock().unwrap();
+                if endpoints.contains(&endpoint) {
+                    async move {
+                        Ok::<_, warp::Rejection>(format!("handling request for: {}", endpoint))
+                    }
+                }
+                else {
+                    async move {
+                        Err(warp::reject::not_found())
+                    }
+                }
+            })
+    }
+
+    let config_route = {
+        let instances_clone = Arc::clone(&instances);
+        let endpoints_clone = Arc::clone(&endpoints);
+
+        warp::path("instances") 
             .and(warp::post())
             .and(warp::body::bytes()) 
             .map(move |body: bytes::Bytes| {
 
+                // NOTE: deserialize
                 match bincode::deserialize::<Hexane>(&body) {
+
                     Ok(des) => {
-                        let mut data_guard = config_clone.lock().unwrap();
+                        let mut data_guard = instances_clone.lock().unwrap();
                         data_guard.push(des.clone());
 
                         if let Some(network) = &des.network_cfg {
                             match &network.options {
 
                                 NetworkOptions::Http(http) => {
-                                    let endpoints = &http.endpoints;
+                                    let mut endpoint_guard = endpoints.lock().unwrap();
+                                    for endpoint in &http.endpoints {
+                                        endpoint_guard.insert(endpoint.clone());
+                                    }
 
                                     println!("http config recieved");
                                     warp::reply::with_status("http config received", warp::http::StatusCode::OK);
@@ -46,6 +79,7 @@ async fn main() {
                             warp::reply::with_status(format!("error: missing network configuration: {:?}", e), warp::http::StatusCode::BAD_REQUEST);
                         }
                     }
+
                     Err(e) => {
                         eprintln!("failed to deserialize hexane config: {:?}", e); 
                         warp::reply::with_status(format!("failed to deserialize hexane config: {:?}", e), warp::http::StatusCode::BAD_REQUEST);
@@ -53,5 +87,11 @@ async fn main() {
                 }
             });
     };
+
+    let routes = base_route
+        .or(dynamic_routes)
+        .or(config_route);
+
+    warp::serve(routines).run(([127, 0, 0, 1], 3000)).await;
 
 }
