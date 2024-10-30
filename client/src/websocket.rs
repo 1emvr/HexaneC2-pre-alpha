e1255c6c90b38e62c274ac9cca9113286b290f45
1emvr <1emvr@protonmail.com>
2024-10-30 06:56:04 -0400

    updating client websocket

--- client/src/websocket.rs
+++ client/src/websocket.rs
@@ -1,26 +1,30 @@
+mod error;
 mod types;
-use crate::error::{Result, Error};
 
+type Error = crate::error::Error;
+type Result<T> = crate::error::Result<T, Rejection>;
 
-//#[derive(serde::Deserialize, serde::Serialize)]
-//pub struct RegisterRequest {
-//    user_id: usize,
-//}
-
-//#[derive(serde::Deserialize, serde::Serialize)]
-//pub struct RegisterResponse {
-//    url: String,
-//}
+use mpsc::UnboundedSender;
+use warp::Error as WarpError;
+use warp::filters::ws::Message;
+use std::{sync, collections};
 
 #[derive(serde::Deserialize, serde::Serialize)]
 pub struct Event {
-    msg_type: u32,
+    etype:    u32,
     user_id:  Option<usize>,
     message:  String,
 }
 
 pub struct Client {
     pub user_id: usize,
-    pub sender: Option<mpsc::UnboundedSender<Result<Message, warp::Error>>>,
+    pub sender:  Option<UnboundedSender<Result<Message, WarpError>>>,
+}
+type Clients = Arc<Mutex<HashMap<String, Client>>>
+
+
+#[tokio::main]
+async fn main() {
+    
 }
 
