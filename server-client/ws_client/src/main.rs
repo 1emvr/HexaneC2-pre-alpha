91157b856d8afa00237241c8cdc2d12990b7a821
1emvr <1emvr@protonmail.com>
2024-11-03 02:59:40 -0500

    updating ws_client

--- server-client/ws_client/src/main.rs
+++ server-client/ws_client/src/main.rs
@@ -1,49 +1,29 @@
-use tokio::net::TcpStream;
-use tokio::io::{self, AsyncWriteExt};
+use url::Url;
+use tungstenite::{connect, Message};
+
 use serde::ser::{Serialize, Serializer, SerializeStruct};
 use serde_json;
 
 use hexlib::stream::Stream;
-use hexlib::types::{Hexane, MessageType};
+use hexlib::types::{HexaneStream, MessageType, NetworkType};
 
+fn main() {
+	let (mut socket, response) = connect(Url::parse("ws://127.0.0.1:3000").unwrap())
+		.expect("cannot connect");
 
-#[tokio::main]
-async fn main() -> io::Result<()> {
-    let data = Hexane {
-        taskid: 1,
-        peer_id: 1234,
-        group_id: 4321,
-        build_type: 1,
-        session_key: vec![1, 2, 3, 4],
-        shellcode: vec![5, 6, 7, 8],
-        config: vec![9, 10, 11, 12],
-        active: true,
-        main_cfg: Default::default(),
-        builder_cfg: Default::default(),
-        compiler_cfg: Default::default(),
-        network_cfg: None,
-        loader_cfg: None,
-        user_session: Default::default(),
-    };
+	socket.write_message(Message::Text("hello, ws_server".into()))
+		.unwrap();
 
-    let serial = serde_json::to_vec(&data)
-        .expect("failed to serialize hexane");
+	loop {
+		let msg = socket.read_message().expect("error reading message");
+		let msg = match msg {
+			tungstenite::Message::Text(s) => s,
+			_ => panic!()
+		};
 
-    let mut stream = Stream::new();
+		let parsed: serde_json::Value = serde_json::from_str(&msg)
+			.expect("cannot parse server response");
 
-    let peer_id = 1234;
-    let task_id = 1;
-    let msg_length = serial.len() as u32;
-
-    stream.create_header(peer_id, task_id, MessageType::TypeConfig as u32);
-    stream.pack_uint32(msg_length);
-    stream.pack_bytes(&serial);
-
-    let mut socket = TcpStream::connect("127.0.0.1:3000").await?;
-    println!("connected to server");
-
-    socket.write_all(stream.get_buffer()).await?;
-    println!("data sent to server");
-
-    Ok(())
+		println!("{:?}", parsed["result"]);
+	}
 }
