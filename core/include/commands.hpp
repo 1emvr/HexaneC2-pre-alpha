8fea84ef8f420fe2d51436cfdcab772ae5d3e01a
1emvr <lemur.proxymslns@outlook.com>
2025-11-07 12:50:04 -0500

    cleanup

--- core/include/commands.hpp
+++ core/include/commands.hpp
@@ -4,33 +4,13 @@
 #include <core/dotnet.hpp>
 
 namespace Commands {
-    VOID
-    FUNCTION
-        ProcessList();
-
-    VOID
-    FUNCTION
-        DirectoryList(PARSER *parser);
-
-    VOID
-    FUNCTION
-        ProcessModules(PARSER *parser);
-
-    VOID
-    FUNCTION
-        CommandAddPeer(PARSER *parser);
-
-    VOID
-    FUNCTION
-        CommandRemovePeer(PARSER *parser);
-
-    VOID
-    FUNCTION
-        Shutdown(PARSER *parser);
-
-    UINT_PTR
-    FUNCTION
-        FindCommandAddress(UINT32 name);
+    VOID ProcessList();
+    VOID DirectoryList(PARSER *parser);
+    VOID ProcessModules(PARSER *parser);
+    VOID CommandAddPeer(PARSER *parser);
+    VOID CommandRemovePeer(PARSER *parser);
+    VOID Shutdown(PARSER *parser);
+    UINT_PTR FindCommandAddress(UINT32 name);
 }
 
 #endif //HEXANE_CORELIB_COMMANDS_HPP
